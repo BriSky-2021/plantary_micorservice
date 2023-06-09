fcd7bdd11e425f3cb7565a1a186fc38bba262d6e
Rupert-WLLP-Bai <2052526@tongji.edu.cn>
2023-06-09 23:35:41 +0800

    test: 加入`allure` 和 `UserDao` 的测试代码

---
+++ README_TEST.md
@@ -0,0 +1,10 @@
+# 软件测试 - 说明
+
+## 测试工具
+
+1. 缺陷跟踪: GitHub Issues
+2. 测试报告生成: Allure
+3. 单元测试: JUnit5
+4. 集成测试: JUnit5
+5. 系统测试: ApiFox
+6. 性能测试: JMeter
---
+++ UserService/src/test/java/edu/tongji/plantary/user/dao/UserDaoTest.java
@@ -0,0 +1,148 @@
+package edu.tongji.plantary.user.dao;
+
+import edu.tongji.plantary.user.dao.UserDao;
+import edu.tongji.plantary.user.entity.User;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import static org.junit.jupiter.api.Assertions.*;
+import io.qameta.allure.junit5.AllureJunit5;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@ExtendWith(AllureJunit5.class)
+@Epic("User DAO Tests Epic")
+@Feature("User DAO Feature")
+public class UserDaoTest {
+
+    @Mock
+    private UserDao userDao;
+
+    private final List<User> users = new ArrayList<>(2);
+
+    @BeforeEach
+    void setup(){
+        // 模拟生成一个UserDao对象
+        userDao = mock(UserDao.class);
+        // 模拟生成一个User对象
+        User user = new User();
+        user.setName("test");
+        user.setPhone("123456789");
+        user.setAge(21);
+        user.setPasswd("test");
+        user.setSex("男");
+        users.add(user);
+        // 模拟生成一个User对象
+        User user2 = new User();
+        user2.setName("test2");
+        user2.setPhone("123456789");
+        user2.setAge(21);
+        user2.setPasswd("test2");
+        user2.setSex("女");
+        users.add(user2);
+    }
+
+    @Test
+    @DisplayName("Find by Name Test")
+    @Description("Test the findByName method in UserDao")
+    @Story("User DAO")
+    void testFindByName() {
+        // Arrange
+        String name = "test";
+        when(userDao.findByName(name)).thenReturn(users);
+
+        // Act
+        List<User> result = userDao.findByName(name);
+
+        // Assert
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        assertEquals("test", result.get(0).getName());
+        assertEquals("test2", result.get(1).getName());
+
+        // Verify the interaction with the userDao mock
+        verify(userDao, times(1)).findByName(name);
+    }
+
+    @Test
+    @DisplayName("Validate Password Test")
+    @Description("Test the validatePasswd method in UserDao")
+    @Story("User DAO")
+    void testValidatePasswd() {
+        // Arrange
+        String phone = "123456789";
+        String passwd = "test";
+        User user = new User();
+        user.setPhone(phone);
+        user.setPasswd(passwd);
+        Optional<User> optionalUser = Optional.of(user);
+        when(userDao.validatePasswd(phone, passwd)).thenReturn(optionalUser);
+
+        // Act
+        Optional<User> result = userDao.validatePasswd(phone, passwd);
+
+        // Assert
+        assertTrue(result.isPresent());
+        assertEquals(phone, result.get().getPhone());
+        assertEquals(passwd, result.get().getPasswd());
+
+        // Verify the interaction with the userDao mock
+        verify(userDao, times(1)).validatePasswd(phone, passwd);
+    }
+
+    @Test
+    @DisplayName("Find by Phone Test")
+    @Description("Test the findByPhone method in UserDao")
+    @Story("User DAO")
+    void testFindByPhone() {
+        // Arrange
+        String phone = "123456789";
+        User user = new User();
+        user.setPhone(phone);
+        Optional<User> optionalUser = Optional.of(user);
+        when(userDao.findByPhone(phone)).thenReturn(optionalUser);
+
+        // Act
+        Optional<User> result = userDao.findByPhone(phone);
+
+        // Assert
+        assertTrue(result.isPresent());
+        assertEquals(phone, result.get().getPhone());
+
+        // Verify the interaction with the userDao mock
+        verify(userDao, times(1)).findByPhone(phone);
+    }
+
+    @Test
+    @DisplayName("Find by Password Test")
+    @Description("Test the findByPasswd method in UserDao")
+    @Story("User DAO")
+    void testFindByPasswd() {
+        // Arrange
+        String passwd = "test";
+        when(userDao.findByPasswd(passwd)).thenReturn(users);
+
+        // Act
+        List<User> result = userDao.findByPasswd(passwd);
+
+        // Assert
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        assertEquals("test", result.get(0).getPasswd());
+        assertEquals("test2", result.get(1).getPasswd());
+
+        // Verify the interaction with the userDao mock
+        verify(userDao, times(1)).findByPasswd(passwd);
+    }
+
+}
